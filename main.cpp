f658d5076c3cfd4386c26ee04c0f9115a56abded
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2021-12-10 20:01:18 +0530

    Create a main.cpp file with code to get some info

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include"funct_definations.h" // for function defination
+
+int main()
+{
+    std::cout << "Welcome to \"GIT BANK\"\n"
+              << "Thank you for choosing us\n";                       // Message to greet the user
+    std::cout << "Let's start by getting some general information\n"; // instructions to the user
+    get_info();                                                     // function call
+    return 0;
+}
